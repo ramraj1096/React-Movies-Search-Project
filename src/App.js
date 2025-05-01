7f15e9fa6f5fa5f01ebc5b2c8ede82bc266fdef6
Ram Raj <142677132+ramraj1096@users.noreply.github.com>
2025-05-01 16:45:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import searchIcon from './search.svg';
 
 
-const API_URL = "http://www.omdbapi.com?apikey=1ae7139a";
+const API_URL = "https://www.omdbapi.com?apikey=1ae7139a";
 const movie1 = {
     
         "Title": "Batman Begins",
